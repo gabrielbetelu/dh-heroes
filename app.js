b45bbce9c3c4e45a337f4167099d07a71dec0e9f
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-04-26 15:40:25 -0300

    subiendo archivos de nuestro sitio y agregamos los 'path' a cada vinculo

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require ('express');
+const path = require ('path');
 const app = express();
+
 app.listen(3030 , () => console.log("Se inicializó el Servidor"));
-app.get('/' , function (req , res) { 
-    res.send("Ingerso al sitio")
-});
+app.get('/' , (req , res) => res.sendFile(path.join(__dirname , '/index.html')));
+app.get('/home' , (req , res) => res.sendFile(path.join(__dirname , '/index.html')));
+app.get('/babbage' , (req , res) => res.sendFile(path.join(__dirname , '/views/babbage.html')));
+app.get('/berners-lee' , (req , res) => res.sendFile(path.join(__dirname , '/views/berners-lee.html')));
+app.get('/clarke' , (req , res) => res.sendFile(path.join(__dirname , '/views/clarke.html')));
+app.get('/hamilton' , (req , res) => res.sendFile(path.join(__dirname , '/views/hamilton.html')));
+app.get('/hopper' , (req , res) => res.sendFile(path.join(__dirname , '/views/hopper.html')));
+app.get('/lovelace' , (req , res) => res.sendFile(path.join(__dirname , '/views/lovelace.html')));
+app.get('/turing' , (req , res) => res.sendFile(path.join(__dirname , '/views/turing.html')));
+
+app.use(express.static('public'));
